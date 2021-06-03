09cb21a7342cf4821760be6cc79d7d5068eba9ec
Leonard Schaerfen <ls2286@c15n02.farnam.hpc.yale.internal>
2021-06-03 12:19:12 -0400

    updated with newest functions

--- summary_workflows/quantification/Q2/benchmarking_dockers/apaeval_metrics/Leo_MatchPAsites_GT/Leo_MatchPAsites_GT.py
+++ summary_workflows/quantification/Q2/benchmarking_dockers/apaeval_metrics/Leo_MatchPAsites_GT/Leo_MatchPAsites_GT.py
@@ -6,103 +6,46 @@ from scipy.stats import pearsonr
 from shutil import which
 import argparse
 
-#parser = argparse.ArgumentParser()
-
-#parser.add_argument('a', help='The BED file containing predictions. MUST be BED6 format.')
-#parser.add_argument('b', help='The ground truth bed file. First 6 columns must be standard BED6, but can have additional columns appended.')
-#parser.add_argument('window', help='Number of bases to append to each side of the predicted site.', type=int)
-#parser.add_argument('-o', help='output file directory') # not yet implemented!!
-#args = parser.parse_args()
-
-#f_GT = args.b
-#f_PD = args.a
-#window = args.window
-
-
-def bedtools_window(bed1, bed2, window):
+def bedtools_window(bed1, bed2, window, reverse=False):
     """
     Python wrapper for bedtools window.
+    reverse: return only sites that have no match in the ground truth.
     """
     
     # make sure bedtools can be called in current env
     assert which('bedtools') is not None, "bedtools not installed or not in PATH"
 
     # run bedtools window, capture output
-    out = subprocess.run(['bedtools', 'window', '-sm',
-                          '-w', str(window), 
-                          '-a', bed1, 
-                          '-b', bed2], 
-                         capture_output=True, shell=False)
+    if not reverse:
+        out = subprocess.run(['bedtools', 'window', '-sm',
+                              '-w', str(window), 
+                              '-a', bed1, 
+                              '-b', bed2], 
+                             capture_output=True, shell=False)
+    else:
+        out = subprocess.run(['bedtools', 'window', '-sm', '-v',
+                              '-w', str(window), 
+                              '-a', bed1, 
+                              '-b', bed2], 
+                             capture_output=True, shell=False)
 
     assert out.returncode==0, "bedtools window run failed, check input files"
 
     # memory file-handle to pass output to pandas without writing to disk
     out_handle = StringIO(out.stdout.decode())
     out = pd.read_csv(out_handle, delimiter='\t', header=None, dtype={0: str})
-    
+        
+    # label columns
+    out.rename({0: 'chrom_p', 1: 'chromStart_p', 2: 'chromEnd_p', 3: 'name_p', 4: 'score_p', 5: 'strand_p', 6: 'chrom_g', 7: 'chromStart_g', 8: 'chromEnd_g', 9: 'name_g', 10: 'score_g', 11: 'strand_g'}, axis=1, inplace=True)
+   
     return(out)
 
-def overlap(coords1, coords2):
-    """
-    Calculates number of points overlapping when the input are two intervals as tuples.
-    Input must be coords1=(int, int),  coords2=(int, int).
-    """
-    return(len(np.intersect1d(np.arange(coords1[0], coords1[1]), np.arange(coords2[0], coords2[1]))))
 
+def match_wtih_gt(f_PD, f_GT, window):
 
-## sum expression levels for predicted sites that overlap with each other given the window size
-## assuming the prediction bed file has exactly 6 columns in the correct order, no header!
+    # bedtools window with specified parameter
+    out = bedtools_window(f_PD, f_GT, window)
 
-# find sites that overlap within the predict set itself when the window is extended
-
-def match_wtih_gt(f_PD,f_GT,window):
-    out = bedtools_window(f_PD, f_PD, window)
-
-    # for logging
-    len_dup = len(out)
-
-    not_unq_mask = out.duplicated([0, 1, 2, 5], keep=False)
-
-    MERGED=False
-    if np.sum(not_unq_mask) > 0: # otherwise there were no overlapping sites
-        MERGED=True
-    
-        not_unq = out[not_unq_mask]
-
-        # remove non-unique sites
-        out.drop(out.index[not_unq_mask], inplace=True, axis=0)
-        out.drop([6, 7, 8, 9, 10, 11], inplace=True, axis=1)
-
-        # collapse non-unique sites, sum up expression, add back to bed file
-        unq = np.unique([(i, j, k, l) for i, j, k, l in zip(not_unq[0], not_unq[1], not_unq[2], not_unq[5])], axis=0)
-
-        for i in unq:
-            non_unq_context = not_unq[(not_unq[0]==i[0]) & (not_unq[1]==int(i[1])) & (not_unq[2]==int(i[2])) & (not_unq[5]==i[3])]
-
-            merged_start = min(list(non_unq_context[7]))
-            merged_end = max(list(non_unq_context[8]))
-            merged_exp = sum(non_unq_context[10])
-
-            out = out.append({0: i[0], 1: merged_start, 2: merged_end, 3: list(non_unq_context[3])[0], 4: merged_exp, 5: i[3]}, ignore_index=True)
-
-        # de-duplicate, sort
-        out.drop_duplicates([0, 1, 2, 5], inplace=True)
-        out.sort_values(by=[0, 1, 2], inplace=True, ascending=[True, True, True])
-    
-        f_PD_new = f_PD[:-4] + '_merged_' + str(window) + '.bed'
-        out.to_csv(f_PD_new, sep='\t', header=None, index=False)
-    
-        print('Wrote intermediate file ' + f_PD_new + '.')
-        print('Merged %i overlapping sites into %i sites.'%(len(unq), len_dup-len(unq)-len(out)))
-
-
-# matching ground truth with predicted (potentially merged) sites
-## assuming the ground truth bed file has 6 columns, can have 1 additional column at the end with the gene name
-
-    if not MERGED:
-        out = bedtools_window(f_PD, f_GT, window)
-    else:
-        out = bedtools_window(f_PD_new, f_GT, window)
 
     # initialize weight column with default 1 for a perfect match
     out['weight'] = [1]*len(out)
@@ -111,86 +54,56 @@ def match_wtih_gt(f_PD,f_GT,window):
     weights_counter = 0
 
     # find rows in sample that are not unique 
-    # -> multiple ground truth sites overlapping one predicted site
-    not_unq_mask = out.duplicated([0, 1, 2, 5], keep=False)
+    # -> extended predicted site overlaps multiple ground truth sites
+    not_unq_mask = out.duplicated(['chrom_p', 'chromStart_p', 'chromEnd_p', 'strand_p'], keep=False)
 
     if np.sum(not_unq_mask) > 0: # otherwise there was no overlap
-        
+            
         not_unq = out[not_unq_mask]
-    
+        
         # remove non-unique sites
         out.drop(out.index[not_unq_mask], inplace=True, axis=0)
-        
-        # collapse non-unique sites, sum up expression, add back to bed file
-        unq = np.unique([(i, j, k, l) for i, j, k, l in zip(not_unq[0], not_unq[1], not_unq[2], not_unq[5])], axis=0)
+            
+        # get the names of non-unique columns
+        unq = np.unique([(i, j, k, l) for i, j, k, l in zip(not_unq['chrom_p'], not_unq['chromStart_p'], not_unq['chromEnd_p'], not_unq['strand_p'])], axis=0)
 
         for i in unq:
-            non_unq_context = not_unq[(not_unq[0]==i[0]) & (not_unq[1]==int(i[1])) & (not_unq[2]==int(i[2])) & (not_unq[5]==i[3])].copy()
-        
-            olps = []
-            for idx, gt_site in non_unq_context.iterrows():
-                frac_overlap = overlap((gt_site[1]-window, gt_site[2]+window), (gt_site[7]-window, gt_site[8]+window))/(gt_site[2]-gt_site[1]+2*window)
-                olps.append(frac_overlap)
-                weights_counter += 1
-        
-            weights = np.array(olps)/np.sum(olps)
-            non_unq_context['weight'] = weights
-        
-            # add back the removed sites including weights
-            out = out.append(non_unq_context)
+            ###### not implemented yet #######
 
-        # sort
-        out.sort_values(by=[0, 1, 2, 7], inplace=True, ascending=[True, True, True, True])
+            # split expression between ground truth sites, according to vicinity
+
+            # re-name sites that have been split
+
+            pass
+
+
+    # find sites with no overlap given the window
+    out_rev = bedtools_window(f_PD, f_GT, window, reverse=True)
+
+    # for non-overlap sites, assign score 0 to ground truth and set other columns to values from prediction
+    out_rev['chrom_g'], out_rev['chromStart_g'], out_rev['chromEnd_g'], out_rev['name_g'], out_rev['score_g'], out_rev['strand_g'], out_rev['weight'] = [out_rev['chrom_p'], out_rev['chromStart_p'], out_rev['chromEnd_p'], out_rev['name_p'], [0.0]*len(out_rev), out_rev['strand_p'], [1.0]*len(out_rev)]
+
+    # add non-matched sites and matched sites together
+    out = pd.concat([out, out_rev])
+
+    # sort
+    out.sort_values(by=['chrom_p', 'chromStart_p', 'chromEnd_p', 'chromStart_g'], inplace=True, ascending=[True, True, True, True])
+
+    # number of matched sites:
+    n_matched_sites = len(len(out)-len(out_rev))
+    # number of sites not matched:
+    n_unmatched_sites = len(out_rev)
     
-    # find rows in ground truth that are not unique 
-    # -> multiple predicted sites overlapping one ground truth site
-    not_unq_mask = out.duplicated([6, 7, 8, 11, 'weight'], keep=False)
-
-    if np.sum(not_unq_mask) > 0: # otherwise there was no overlap
-        
-        not_unq = out[not_unq_mask]
-    
-        # remove non-unique sites
-        out.drop(out.index[not_unq_mask], inplace=True, axis=0)
-
-        # collapse non-unique sites, sum up expression, add back to bed file
-        unq = np.unique([(i, j, k, l) for i, j, k, l in zip(not_unq[6], not_unq[7], not_unq[8], not_unq[11])], axis=0)
-    
-        for i in unq:
-            non_unq_context = not_unq[(not_unq[6]==i[0]) & (not_unq[7]==int(i[1])) & (not_unq[8]==int(i[2])) & (not_unq[11]==i[3])].copy()
-        
-            olps = []
-            for idx, gt_site in non_unq_context.iterrows():
-                frac_overlap = overlap((gt_site[1]-window, gt_site[2]+window), (gt_site[7]-window, gt_site[8]+window))/(gt_site[2]-gt_site[1]+2*window)
-                olps.append(frac_overlap)
-                weights_counter += 1
-        
-            weights = np.array(olps)/np.sum(olps)
-            non_unq_context['weight'] = weights
-        
-            # add back the removed sites including weights
-            out = out.append(non_unq_context)
-
-        # sort
-        out.sort_values(by=[0, 1, 2, 7], inplace=True, ascending=[True, True, True, True])
-
-    f_PD_matched = f_PD[:-4] + '_matched_' + str(window) + '.bed'
- #   out.to_csv(f_PD_matched, sep='\t', header=None, index=False)
- #   print('Wrote output to file ' + f_PD_matched)
- #   print('Matched %i sites in total.'%(len(out)))
- #   print('Added weights to %i overlapping sites.'%(weights_counter))
-    matched_sites=len(out)
-    return (out, matched_sites)
+    return(out, n_matched_sites, n_unmatched_sites)
 
 def corr_with_gt(out):
-#    out = pd.read_csv(fname, delimiter='\t', header=None)
 
     # initialize vectors for ground truth sites and prediction
     vec_true = []
     vec_pred = []
 
     # multiple predicted sites for one ground truth?
-    multiple_predicted_sites = out.duplicated([6, 7, 8, 11], keep=False)
+    multiple_predicted_sites = out.duplicated(['chrom_g', 'chromStart_g', 'chromEnd_g', 'strand_g'], keep=False)
 
     # iterate over matched sites
     for (idx, row), is_mult in zip(out.iterrows(), multiple_predicted_sites):
@@ -199,15 +112,12 @@ def corr_with_gt(out):
             # needs to be implemented or can be skipped for now since these are usually only a few
             pass
         else:
-            vec_true.append(row[10])
+            vec_true.append(row['score_g'])
             # weighted expression in case there are multiple ground truth sites for one predicted site
-            vec_pred.append(row[4]*row['weight'])
+            vec_pred.append(row['score_p']*row['weight'])
 
     # correlation coefficient
     r = pearsonr(vec_true, vec_pred)[0]
 
-    # export json
- #   with open(fname[:-4] + '_corr.json', 'w') as f:
- #       json.dump({'correlation_coefficient': r}, f)
- #  print('correlation coefficient: %.2f'%(r))
-    return r
+    return(r)
+
