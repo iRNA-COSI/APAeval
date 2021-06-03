2bbe4ab2489868ee49bbdb7527f4ec6e0a12bb18
mrgazzara <58347523+mrgazzara@users.noreply.github.com>
2021-06-03 18:37:12 -0400

    Update make_quant_bed.py
    `ln[8]` and `ln[9]` correspond to "UTR3.Start" and "UTR3.End", respectively. These are genomic coordinates and so depending on the strand the actual PAS coordinate will change to being either the end of the transcript (`if strand=="+"` we need the "UTR3.End" / `ln[8]. `if strand =="-"` then we need "UTR3.Start" / `ln[8]`)

--- execution_workflows/QAPA/bin/make_quant_bed.py
+++ execution_workflows/QAPA/bin/make_quant_bed.py
@@ -9,6 +9,13 @@ header_fields=['chrom','chromStart','chromEnd','name','score','strand']
 qapa_quant_bed.write('\t'.join(header_fields)+'\n')
 for ln in qapa_results_file:
  ln=ln.strip().split('\t')
- fields=[ln[4],ln[8],ln[9],ln[0],ln[13],ln[7]]
+ strand = ln[7] 
+ #if "+" strand gene we want to grab "UTR3.End" as PAS
+ if strand == '+': 
+  pas_coord = int(ln[9])
+ #if "-" strand gene we need "UTR3.Start" as PAS
+ else: 
+  pas_coord = int(ln[8])
+ fields=[ln[4], str(pas_coord-1), str(pas_coord),ln[0],ln[13],ln[7]]
  qapa_quant_bed.write('\t'.join(fields)+'\n')
 qapa_quant_bed.close()
