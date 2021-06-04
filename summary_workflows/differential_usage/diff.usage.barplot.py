ed9f86bf76d760bb20b1484e5853de4d4a30d240
chilampoon <clpoon807@gmail.com>
2021-06-03 22:55:38 -0400

    changed the dir argument to multi files and minor corrections

--- summary_workflows/differential_usage/diff.usage.barplot.py
+++ summary_workflows/differential_usage/diff.usage.barplot.py
@@ -5,10 +5,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-def bar_plot(df_path, plot_out):
+def bar_plot(files, plot_out):
 	'''
 	Inputs:
-		- df_path: path to a directory of tsv files containing 
+		- files: a list of tsv files containing 
 			  	gene_id & usage changes (e.g. deltaPPAU in QAPA);
 			  	NOTE that these genes have been filtered by FDR/q-value.
 			  	File names are like 'AA_diffUTR_03.tsv'
@@ -17,22 +17,21 @@ def bar_plot(df_path, plot_out):
 		- output: path to store the image
 	'''
 
-	files = os.listdir(df_path)
 	files = [f for f in files if '_03.tsv' in f]
-
 	methods = {}
 	for file in files:
-		method = re.sub('_03.tsv$', '', file)
+		method = re.sub('_03.tsv$', '', os.path.basename(file))
 		method = re.sub('^.*_', '', method)
 
-		# hardcode the cutoffs
+		# hardcode the cutoffs since each method has its own calculation
+		cutoff = 0
 		if method == "QAPA":
 			cutoff = 20
 		elif method == "diffUTR":
-			cutoff = 0.5
+			cutoff = 0.5 #not sure, can ask the author
 		# others to be added
 
-		df = pd.read_csv(os.path.join(df_path, file), sep='\t', header=None, names=['gene','usage'])
+		df = pd.read_csv(file, sep='\t', header=None, names=['gene','usage'])
 		l = df[df.usage >= cutoff].shape[0]
 		s = df[df.usage <= -cutoff].shape[0]
 		nc = df.shape[0] - l - s
@@ -45,20 +44,20 @@ def bar_plot(df_path, plot_out):
 
 
 def main(args):
-	df_path = args['pathToTSV'][0] # directory to tsv files
-	out_path = args['out'][0] # directory to output image
-	plot_out = out_path + '/diff.usages.pdf'
+	files = args['tsvfile'] # tsv files
+	out_path = args['outdir'][0] # directory to output image
+	plot_out = os.path.join(out_path, 'diff.usages.pdf')
 	#cutoff = float(args['cutoff'][0])
 
-	bar_plot(df_path,plot_out)
+	bar_plot(files, plot_out)
 
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='Differential usage plot')
 	#parser.add_argument('--cutoff', required=True, nargs=1)
-	parser.add_argument('--pathToTSV', required=True, nargs=1)
-	parser.add_argument('--out', required=True, nargs=1)
+	parser.add_argument('--tsvfile', required=True, nargs='+')
+	parser.add_argument('--outdir', required=True, nargs=1)
 	args = vars(parser.parse_args())
 	main(args)
 
-# usage: ./diff.usage.barplot.py --pathToTSV "../" --out "."
+# usage: ./diff.usage.barplot.py --tsvfile ../AA_QAPA_03.tsv ../BB_MISO_03.tsv ../AB_diffUTR_03.tsv  --outdir .
