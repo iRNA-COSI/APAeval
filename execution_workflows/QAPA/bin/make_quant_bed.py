3b8b138499212e8c24d009d3ff83638c2ce4ca00
mrgazzara <58347523+mrgazzara@users.noreply.github.com>
2021-06-04 00:17:06 -0400

    fix: remove header, "chr" in QAPA post-processing
    Removed header and leading "chr" to be consistent with specification for output BED6 files.

--- execution_workflows/QAPA/bin/make_quant_bed.py
+++ execution_workflows/QAPA/bin/make_quant_bed.py
@@ -6,16 +6,16 @@ qapa_results_file=open(sys.argv[1],'r')
 qapa_results_file.readline()
 qapa_quant_bed=open(sys.argv[2],'w')
 header_fields=['chrom','chromStart','chromEnd','name','score','strand']
-qapa_quant_bed.write('\t'.join(header_fields)+'\n')
 for ln in qapa_results_file:
  ln=ln.strip().split('\t')
- strand = ln[7] 
+ strand = ln[7]
+ chrm = ln[4][3:] #specification requires no leading "chr"
  #if "+" strand gene we want to grab "UTR3.End" as PAS
  if strand == '+': 
   pas_coord = int(ln[9])
  #if "-" strand gene we need "UTR3.Start" as PAS
  else: 
   pas_coord = int(ln[8])
- fields=[ln[4], str(pas_coord-1), str(pas_coord),ln[0],ln[13],ln[7]]
+ fields=[chrm, str(pas_coord-1), str(pas_coord),ln[0],ln[13],ln[7]]
  qapa_quant_bed.write('\t'.join(fields)+'\n')
 qapa_quant_bed.close()
