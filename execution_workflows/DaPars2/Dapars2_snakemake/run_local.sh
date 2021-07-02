caf13dfc695b4f0cab30796fa1d8fde7b6e8c1c8
Sam Bryce-Smith <49978382+SamBryce-Smith@users.noreply.github.com>
2021-07-02 12:10:57 +0100

    add bind to test-data dir - fixes new file not found error

--- execution_workflows/DaPars2/Dapars2_snakemake/run_local.sh
+++ execution_workflows/DaPars2/Dapars2_snakemake/run_local.sh
@@ -3,7 +3,8 @@ snakemake \
     --snakefile="workflow/Snakefile" \
     --configfile="config/config.DaPars2.yaml" \
     --cores 4 \
-    --use-conda \
+    --use-singularity \
+    --singularity-args="--bind ${PWD}/../../../tests/test_data" \
     --printshellcmds
 # adjust number as needed
 # or --use-singularity
