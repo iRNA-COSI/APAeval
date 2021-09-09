5a2673ea3b5166174f90f793cba01bd5e511eaff
Yuk Kei Wan <41866052+yuukiiwa@users.noreply.github.com>
2021-09-09 12:01:42 +0800

    updated docker container: https://hub.docker.com/r/apaeval/gtftobed

--- utils/gtfTobed.py
+++ utils/gtfTobed.py
@@ -10,16 +10,20 @@ def main():
     parser = argparse.ArgumentParser(description='GTF to BED')
     parser.add_argument('--gtf', dest='gtf', type=str,
                         help='Input gtf file.', required=True)
+    parser.add_argument('--out_bed', dest='out_bed', type=str,
+                        help='Input gtf file.', required=True)
     args = parser.parse_args()
     gtf=args.gtf
+    bed=args.out_bed
     genePred=".".join(gtf.split(".")[:-1])+".genePred"
-    bed=".".join(gtf.split(".")[:-1])+".bed"
-    cmd1 = ['/app/gtfToGenePred',  gtf, genePred]
+    #bed=".".join(gtf.split(".")[:-1])+".bed"
+    cmd1 = ['/gtfToGenePred',  gtf, genePred]
     print(' '.join(cmd1))
     subprocess.run(' '.join(cmd1), shell=True, check=True)
-    cmd2 = ['/app/genePredToBed', genePred, bed]
+    cmd2 = ['/genePredToBed', genePred, bed]
     print(' '.join(cmd2))
     subprocess.run(' '.join(cmd2), shell=True, check=True)
+    os.remove(genePred)
 
 if __name__ == "__main__":
     main()
--- utils/nextflow_minitest/gtftobed.nf
+++ utils/nextflow_minitest/gtftobed.nf
@@ -2,6 +2,7 @@
 
 nextflow.enable.dsl=2
 gtf=""
+out_bed=""
 
 process gtfToBed{
   container "docker.io/apaeval/gtftobed:1.0"
@@ -9,18 +10,19 @@ process gtfToBed{
 
   input:
   path gtf
+  val out_bed
     
   output:    
   path "*.bed" , emit: bed
 
   script:
   """
-  python3 /app/gtfTobed.py --gtf $gtf
+  /gtfTobed.py --gtf $gtf --out_bed $out_bed
   """
 }
 
 workflow {
   gtfToBed(
-    file(params.gtf)
+    file(params.gtf), params.out_bed
   )
 }
