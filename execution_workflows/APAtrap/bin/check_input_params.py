bdc20656ebc5ad90e85dd4c42175c91010e3d447
Farica Zhuang <faricazjj@gmail.com>
2021-12-29 18:29:03 -0800

    Update check_input_params.py to check for file extension for each mode and exactly 2 conditions for differential mode

--- execution_workflows/APAtrap/bin/check_input_params.py
+++ execution_workflows/APAtrap/bin/check_input_params.py
@@ -11,10 +11,11 @@ This file checks if mode and the extension of the file names set in conf/modules
 
 def parse_args(args=None):
     Description = "Check DePars input parameters"
-    Epilog = "Example usage: python check_input_params.py <IDENTIFICATION_OUT_SUFFIX> <QUANTIFICATION_OUT_SUFFIX>" +\
+    Epilog = "Example usage: python check_input_params.py <SAMPLE_FILE> <IDENTIFICATION_OUT_SUFFIX> <QUANTIFICATION_OUT_SUFFIX>" +\
              " <DIFFERENTIAL_OUT> <RUN_IDENTIFICATION> <RUN_QUANTIFICATION> <RUN_DIFFERENTIAL>"
 
     parser = argparse.ArgumentParser(description=Description, epilog=Epilog)
+    parser.add_argument("SAMPLE_FILE", help="Input sample file for the run.")
     parser.add_argument("IDENTIFICATION_OUT_SUFFIX", help="Name of APAtrap output for the identification challenge.")
     parser.add_argument("QUANTIFICATION_OUT_SUFFIX", help="Name of APAtrap output for the quantification challenge.")
     parser.add_argument("DIFFERENTIAL_OUT", help="Name of APAtrap output for the differential challenge.")
@@ -39,7 +40,12 @@ def main(args=None):
         if not args.FILE_OUT.endswith(".tsv"):
             msg = "The differential output file name should end with '.tsv'"
             sys.exit(msg)
-        # check that there are
+        # check that there are exactly two distinct conditions
+        sample = pd.read_csv(args.SAMPLE_FILE)
+        num_conditions = len(set(sample[:,"condition"]))
+        if num_conditions != 2:
+            msg = "The number of distinct conditions in the input sample file to run differential should be 2.\n"
+            msg += "Got " + str(num_conditions) + "."
 
 
 if __name__ == '__main__':
