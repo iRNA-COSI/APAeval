d43c93a795e38890199fcec2dae210f3f6b6c76e
Sam Bryce-Smith <49978382+SamBryce-Smith@users.noreply.github.com>
2022-01-21 11:16:00 +0000

    drop duplicate polyA coords. Raise error if no aptardi txipts in gtf

--- execution_workflows/Aptardi/bin/make_identification_bed.py
+++ execution_workflows/Aptardi/bin/make_identification_bed.py
@@ -4,14 +4,19 @@ import sys
 import pyranges as pr
 import pandas as pd
 
-input_gtf_path=sys.argv[1]
-output_bed_path=sys.argv[2]
-if output_bed_path == 'not_specified':
- output_bed_path=input_gtf_path.split(".")[0]+"_identification.bed"
+input_gtf_path = sys.argv[1]
+output_bed_path = sys.argv[2]
 
-gtf=pr.read_gtf(input_gtf_path)
+if output_bed_path == 'not_specified':
+    output_bed_path = input_gtf_path.split(".")[0]+"_identification.bed"
+
+gtf = pr.read_gtf(input_gtf_path)
 # Source is 2nd col in GTF, Feature is 3rd col
 gtf = gtf.subset(lambda df: (df["Source"] == "aptardi") & (df["Feature"] == "transcript"))
+
+if len(gtf) == 0:
+    raise Exception("No Aptardi predicted transcripts found in provided GTF file")
+
 # Strand aware, reports 3'most coordinate for each interval
 # PyRanges coordinates follow BED conventions
 three_ends = gtf.three_end()
@@ -20,8 +25,12 @@ three_ends = three_ends[["Strand"]]
 # Dummy score column
 three_ends.Score = "."
 
+# Drop duplicate polyA sites if present
+three_ends = three_ends.drop_duplicate_positions()
+
 # Making up a Name column, can be whatever you like though
 # <chr>:<start>:<end>
 three_ends = three_ends.assign("Name",
-lambda df: df[["Chromosome","Start","End"]].apply(lambda row: ":".join(row.values.astype(str)), axis=1))
+                               lambda df: df[["Chromosome", "Start", "End"]].apply(lambda row: ":".join(row.values.astype(str)), axis=1))
+
 three_ends.to_bed(output_bed_path)
