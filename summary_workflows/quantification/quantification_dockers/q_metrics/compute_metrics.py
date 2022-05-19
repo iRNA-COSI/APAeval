17a357348ff672af3270f3d8dba358a899fd463d
Dominik Burri <dominik.burri19@gmail.com>
2022-05-19 08:55:04 +0200

    feat: quantification metric: relative PAS usage (#238)
    * new aggregation draft script
    
    * use template if no aggregation file present
    
    * add barplot handling
    
    * save aggregation in run result dir instead of benchmark dir
    
    * add mock MSE metric, loop over metrics to create assessment objects
    
    * adapt plotting to multiple aggregation objects
    
    * switch to new aggregation script
    
    * fix handling of list of challenges
    
    * save assessments per challenge in results/challenge_id dir
    
    * refactor: template loading as function
    
    * refactor: reorder steps
    
    * feat: add OEB DB download functionality
    
    * refactor: remove old script manage_assessment_data.py
    
    * Add code to quantification summary workflow to render barplot (#229)
    
    * Reinstate challenge_id
    
    * Add bar-plot aggregation to example aggregation file
    
    * Delete unused compute_resources.json aggregation file
    
    * Add code to render bar plots
    
    * bug: MSE instead of correlation
    
    * use Enum for plot values
    
    * refactor: remove duplicate try
    
    * Ag test swf in vre (#232)
    
    * Rename challenge_ids parameter
    
    The tool used to launch the workflow requires it to be called `challenges_ids`
    
    * Set online mode
    
    * Updated benchmarking event id
    
    Identification: "OEBE0070000001"
    Quantification: "OEBE0070000002"
    Differential Usage: "OEBE0070000003"
    
    * Ag fix vre vis (#235)
    
    * Create one file per aggregation object in results dir for visualisation in VRE
    
    * Rename 'challenge_ids' variable since the VRE requires to be called 'challenges_ids'
    
    * Fix typo in offline parameter
    
    * Read all aggregation files from results dir
    
    * feat:several window sizes in one swf run (#237)
    
    * feat: list of window sizes
    
    * fix: continue if no data obtained from DB
    
    * feat: one aggr object per window size
    
    * add relative pas usage metric. Fixes #231.
    
    * adjust metric ID to expression_unmatched
    
    * avoid division by zero
    
    * use available function for correlation computation
    
    * compute relative PAS usage only for first window size;
    adjust test aggregation for displaying only one MSE plot.
    
    * use genome dir to circumvent additional parameter
    in nextflow call.
    
    * rename test files to properly use genome information.
    
    * add missing dir in path
    
    * adapt genome_file parameter to genome_dir
    
    * change rel usage variable names to correlation
    
    * remove mentions of MSE in relative_pas_usage
    
    * test: add small test files
    
    * refactor: fix metric names
    
    * address issue 240
    
    * fixes
    - metric names
    - use new minimal test data with dedicated genome (renaming)
    - dirty fix for validation: gtf only from first challenge used
         to check for chromosome names
    - nextflow: correctly use genome_dir twice
    
    * change rel. PAS usage calculation to use
    unmatched PD and GT
    (instead of only unmatched GT).
    
    * use "chr" for test files, documentation,
    fix for gtf w/ < 50 rows.
    
    Co-authored-by: CJ Herrmann <ninsch3000@gmail.com>
    Co-authored-by: Asier Gonzalez <asier.gonzalez@bsc.es>
    Co-authored-by: CJ Herrmann <36634279+ninsch3000@users.noreply.github.com>
    Co-authored-by: txellfe <meritxell.ferret@bsc.es>
    Co-authored-by: Sam Bryce-Smith <49978382+SamBryce-Smith@users.noreply.github.com>
    Co-authored-by: Yuk Kei Wan <ywan6@jhu.edu>

--- summary_workflows/quantification/quantification_dockers/q_metrics/compute_metrics.py
+++ summary_workflows/quantification/quantification_dockers/q_metrics/compute_metrics.py
@@ -18,6 +18,7 @@ def main(args):
     community = args.community_name
     out_path = args.output
     windows = args.windows
+    genome_path = args.genome_dir
 
     # Assuring the output path does exist
     if not os.path.exists(os.path.dirname(out_path)):
@@ -28,63 +29,69 @@ def main(args):
         except OSError as exc:
             print("OS error: {0}".format(exc) + "\nCould not create output path: " + out_path)
 
-    compute_metrics(participant_input, gold_standards_dir, challenge_ids, participant, community, out_path, windows)
+    compute_metrics(participant_input, gold_standards_dir, challenge_ids, participant, community, out_path, windows, genome_path)
 
 
-def compute_metrics(participant_input, gold_standards_dir, challenge_ids, participant, community, out_path, windows):
+def compute_metrics(participant_input, gold_standards_dir, challenge_ids, participant, community, out_path, windows, genome_path):
 
     # define array that will hold the full set of assessment datasets
     all_assessments = []
 
     for challenge in challenge_ids:
-        
+
         # ID prefix for assessment objects
         base_id = f"{community}:{challenge}_{participant}_"
         # Dict to store metric names and corresponding variables + stderr (which is currently not computed and set to 0)
         metrics = {}
         # ground truth file
         gold_standard = os.path.join(gold_standards_dir, challenge + ".bed")
+        # genome annotation file
+        genome_file = select_genome_file(challenge, genome_path)
+        # log to stdout
+        print(f"INFO: In challenge {challenge}. Using genome file: {genome_file}")
+        genome = matchPAS.load_genome(genome_file)
 
         for window in windows:
 
-        # METRIC: Matched sites
-        ########################
+            # METRIC: Matched sites
+            ########################
             # metric on the number of matched sites
             match_with_gt_run = matchPAS.match_with_gt(participant_input,gold_standard,window)
             merged_bed_df, expression_unmatched = match_with_gt_run[0], match_with_gt_run[1]
 
             # Key: exact name of metric as it appears in specification
-            metric_name = f"expression_unmatched_{window}nt"
+            metric_name = f"Expression_non-matched-PAS_{window}nt"
             # Value: List of [variable_holding_metric, std_err]
             metrics[metric_name] = [expression_unmatched, 0]
 
 
-            # METRIC: correlation coffecient
+            # METRIC: correlation coefficient
             #################################
-            # metric on correlation coffecient
-            correlation= matchPAS.corr_with_gt(merged_bed_df)
+            # metric on correlation coefficient
+            correlation = matchPAS.corr_with_gt(merged_bed_df)
             # Key: exact name of metric as it appears in specification
-            metric_name = f"correlation_{window}nt"
+            metric_name = f"Correlation_coefficient_{window}nt"
             # Value: List of [variable_holding_metric, std_err]
             metrics[metric_name] = [correlation, 0]
-                    
 
-            # METRIC: MSE
+
+            # METRIC: correlation coefficient of relative pas usage
             ####################
-            # Placeholder:
-            mse = 0.5
-            
-            # Key: exact name of metric as it appears in specification
-            metric_name = f"MSE_{window}nt"
-            # Value: List of [variable_holding_metric, std_err]
-            metrics[metric_name] = [mse, 0]
+            # Only calculate metric for first window size.
+            if window == windows[0]:
+                match_with_gt_and_pd = matchPAS.match_with_gt(participant_input,gold_standard,window, "with_unmatched_GT_and_PD")
+                merged_bed_df_wpd, _ = match_with_gt_and_pd[0], match_with_gt_and_pd[1]
+                correlation_rel_use = matchPAS.relative_pas_usage(merged_bed_df_wpd, genome)
+                # Key: exact name of metric as it appears in specification
+                metric_name = f"Correlation_coefficient_relative_{window}nt"
+                # Value: List of [variable_holding_metric, std_err]
+                metrics[metric_name] = [correlation_rel_use, 0]
 
         # for each challenge, create all assessment json objects and append them to all_assessments
         for key, value in metrics.items():
-            object_id = base_id + key 
+            object_id = base_id + key
             assessment_object = JSON_templates.write_assessment_dataset(object_id, community, challenge, participant, key, value[0], value[1])
             all_assessments.append(assessment_object)
-
 
     # once all assessments have been added, print to json file
     with io.open(out_path,
@@ -92,9 +99,47 @@ def compute_metrics(participant_input, gold_standards_dir, challenge_ids, partic
         jdata = json.dumps(all_assessments, sort_keys=True, indent=4, separators=(',', ': '))
         f.write(jdata)
 
+def select_genome_file(file_name, genome_path):
+    """Select the genome file according to the organism.
+
+    Requires that the file_name contains an expression containing organism
+    information, which will be matched against the genome_path directory.
+    The format should be: name.mm10.ext or name.hg38extension.ext, with
+    matching genome annotations: gencode.mm10.gtf and gencode.hg38extension.gtf.
+    Note: no check for the extension (e.g. gtf) is done.
+
+    Args:
+        file_name (str): Name containing organism information. Supported: mm* and hg*.
+        genome_path (str): directory containing genome annotations in gtf format.
+
+    Returns:
+        str with genome file path.
+    """
+    GENOME_STRINGS = ["mm", "hg"]
+    SPLITSTRING = "."
+    assert os.path.exists(genome_path), f"Genome annotation directory not found: {genome_path}"
+    file_components =  file_name.split(SPLITSTRING)
+    # search for genome
+    for genome_string in GENOME_STRINGS:
+        match = [comp for comp in file_components if genome_string in comp]
+        if len(match) != 0:
+            break
+    if len(match) == 0:
+        raise ValueError(f"No genome string: {GENOME_STRINGS} in file_name: {file_name} found.")
+    # find all genome files in genome_path
+    for f in os.listdir(genome_path):
+        # find exact match in file
+        genome_match = [f for comp in f.split(SPLITSTRING) if match[0] == comp]
+        if len(genome_match) != 0:
+            break
+    if len(genome_match) == 0:
+        raise ValueError(f"No genome string: {GENOME_STRINGS} in genome_path: {genome_path} found.")
+    # return file
+    return os.path.join(genome_path, genome_match[0])
+
 
 if __name__ == '__main__':
-    
+
     parser = ArgumentParser()
     parser.add_argument("-i", "--participant_data", help="execution workflow prediction outputs", required=True)
     parser.add_argument("-c", "--challenge_ids", nargs='+', help="List of challenge ids selected by the user, separated by spaces", required=True)
@@ -102,13 +147,10 @@ if __name__ == '__main__':
     parser.add_argument("-p", "--participant_name", help="name of the tool used for prediction", required=True)
     parser.add_argument("-com", "--community_name", help="name/id of benchmarking community", required=True)
     parser.add_argument("-o", "--output", help="output path where assessment JSON files will be written", required=True)
-    parser.add_argument("-w", "--windows", nargs='+', help="windows (nt) for scanning for poly(A) sites; several window sizes separated by spaces", required=True, type=int)
-    
+    parser.add_argument("-w", "--windows", nargs='+', help="windows (nt) for scanning for poly(A) sites; several window sizes separated by spaces. The first entry is used for MSE calculation.", required=True, type=int)
+    parser.add_argument("-gtf", "--genome_dir", help="genome annotation directory. Used for relative PAS usage calculation. Directory needs to contain genome files with matching organism name from challenge.", required=True)
+
     args = parser.parse_args()
 
-    
+
     main(args)
-
-
-
-
