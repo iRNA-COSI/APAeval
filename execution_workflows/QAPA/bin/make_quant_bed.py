a4c0bfac3d5264dd91a068f05ede882dea7b2935
Yuk Kei Wan <ywan6@jhu.edu>
2022-07-20 15:35:52 +0800

    report PPAU fraction from QAPA

--- execution_workflows/QAPA/bin/make_quant_bed.py
+++ execution_workflows/QAPA/bin/make_quant_bed.py
@@ -8,10 +8,11 @@ qapa_quant_bed=open(sys.argv[2],'w')
 
 for ln in qapa_results_file:
  ln=ln.strip().split('\t')
+ ppau_fraction=float(ln[12])/100.0
  if ln[7] == '+':
   chromStart,chromEnd=str(int(ln[9])-1),ln[9]
  elif ln[7] == '-':
   chromStart,chromEnd=ln[8],str(int(ln[8])+1)
- fields=[ln[4],chromStart,chromEnd,ln[0],ln[13],ln[7]]
+ fields=[ln[4],chromStart,chromEnd,ln[0],str(ppau_fraction),ln[7]]
  qapa_quant_bed.write('\t'.join(fields)+'\n')
 qapa_quant_bed.close()
