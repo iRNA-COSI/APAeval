98331f77893a35eac0b7d6968f74563771e95a34
Farica Zhuang <faricazjj@gmail.com>
2022-07-22 17:05:20 -0400

    update script to check new config parameters

--- execution_workflows/DaPars/bin/check_input_params.py
+++ execution_workflows/DaPars/bin/check_input_params.py
@@ -7,14 +7,15 @@ import pandas as pd
 
 def parse_args(args=None):
     Description = "Check DePars input parameters"
-    Epilog = "Example usage: python check_input_params.py <IDENTIFICATION_OUT_SUFFIX> <DIFFERENTIAL_OUT>" + \
-                " <RUN_IDENTIFICATION> <RUN_DIFFERENTIAL>"
+    Epilog = "Example usage: python check_input_params.py <IDENTIFICATION_OUT_SUFFIX> <RELATIVE_USAGE_QUANTIFICATION_OUT_SUFFIX> <DIFFERENTIAL_OUT>" + \
+                " <RUN_IDENTIFICATION> <RUN_RELATIVE_USAGE_QUANTIFICATION> <RUN_DIFFERENTIAL>"
 
     parser = argparse.ArgumentParser(description=Description, epilog=Epilog)
     parser.add_argument("IDENTIFICATION_OUT_SUFFIX", help="Name of DaPars output for the identification challenge.")
     parser.add_argument("DIFFERENTIAL_OUT", help="Name of DaPars output for the differential challenge.")
-    parser.add_argument("RUN_IDENTIFICATION", help="Can either be 'true' or 'false")
-    parser.add_argument("RUN_DIFFERENTIAL", help="Can either be 'true' or 'false")
+    parser.add_argument("RUN_IDENTIFICATION", help="Can either be 'true' or 'false'")
+    parser.add_argument("RUN_RELATIVE_USAGE_QUANTIFICATION", help="Can either be 'true' or 'false'")
+    parser.add_argument("RUN_DIFFERENTIAL", help="Can either be 'true' or 'false'")
     return parser.parse_args(args)
 
 
@@ -23,8 +24,12 @@ def main(args=None):
     if args.RUN_IDENTIFICATION == 'true':
         if not args.IDENTIFICATION_OUT_SUFFIX.endswith(".bed"):
             msg = "The identification output file name should end with '.bed'"
+            sys.exit(msg) 
+    if args.RUN_RELATIVE_USAGE_QUANTIFICATION == 'true':
+        if not args.IDENTIFICATION_OUT_SUFFIX.endswith(".bed"):
+            msg = "The relative usage quantification output file name should end with '.bed'"
             sys.exit(msg)
-    elif args.RUN_DIFFERENTIAL == 'true':
+    if args.RUN_DIFFERENTIAL == 'true':
         if not args.DIFFERENTIAL_OUT.endswith(".tsv"):
             msg = "The differential output file name should end with '.tsv'"
             sys.exit(msg)
