cdf37c3993c7c78925c8758397bb188100185a47
pjewell <pjewell@biociphers.org>
2022-08-03 14:01:14 -0400

    -added JSON_templates as a package

---
+++ summary_workflows/JSON_templates/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    packages=find_packages(where="src"),
+    package_dir={'': 'src'},
+    install_requires=['jsonschema']
+)
---
+++ summary_workflows/JSON_templates/src/JSON_templates/__init__.py
@@ -0,0 +1,1 @@
+from JSON_templates.main import *
