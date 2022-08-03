965c0cc1542ecd2565134e1f985a3b9db4128e1f
pjewell <pjewell@biociphers.org>
2022-08-03 14:10:52 -0400

    -added package data to JSON_templates package

--- summary_workflows/JSON_templates/setup.py
+++ summary_workflows/JSON_templates/setup.py
@@ -3,5 +3,6 @@ from setuptools import setup, find_packages
 setup(
     packages=find_packages(where="src"),
     package_dir={'': 'src'},
-    install_requires=['jsonschema']
+    install_requires=['jsonschema'],
+    package_data={"": ["*.json"]}
 )
