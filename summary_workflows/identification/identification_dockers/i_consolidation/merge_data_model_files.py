c9c73a4ef38506003d519c4d7129377ed439cdab
CJ Herrmann <ninsch3000@gmail.com>
2022-09-09 11:12:42 +0200

    initial commit: copy from q and adapt  to i

---
+++ summary_workflows/identification/identification_dockers/i_consolidation/merge_data_model_files.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+
+import io
+import json
+import os
+import fnmatch
+from argparse import ArgumentParser
+
+
+def main(args):
+    # input parameters
+    metrics_data = args.metrics_data
+    validation_data = args.validation_data
+    aggregation_data = args.aggregation_data
+    challenges = args.challenge_ids
+    out_path = args.output
+    print(out_path)
+
+    # Nextflow passes list, python reads string. We need python lists
+    metrics_data = [m.strip('[').strip(']').strip(',') for m in metrics_data]
+    validation_data = [v.strip('[').strip(']').strip(',') for v in validation_data]
+            
+    # This is the final consolidated output
+    data_model_file = []
+
+    # get the output files from previous steps and concatenate
+    # from validation ("validated_participant_data")
+    for v in validation_data:
+        data_model_file = join_json_files(v, data_model_file, "*.json")
+    # from metrics ("assessment_out")
+    for m in metrics_data:
+        data_model_file = join_json_files(m, data_model_file, "*.json")
+    # from consolidation part 1 (manage_assessment_data.py), "sample_out/results/challenge/challenge.json"
+    # we have to do that for all challenges in the list
+    for challenge in challenges:
+        challenge = challenge.replace('.', '_')
+        c_aggregation_data = os.path.join(aggregation_data, challenge)
+        data_model_file = join_json_files(c_aggregation_data, data_model_file, "*" + challenge + "*.json")
+
+    # write the merged data model file to json output
+    with open(out_path, mode='w', encoding="utf-8") as f:
+        json.dump(data_model_file, f, sort_keys=True, indent=4, separators=(',', ': '))
+
+def join_json_files(data_directory, data_model_file, file_extension):
+    '''Add contents of specified file(s) to given json file
+    Input:
+    data_directory: Directory containing the file(s) to be added
+    data_model_file: list to be extended with json objects (dicts)
+    file_extension: filename or pattern to be matched, of the file(s) to be added
+    Returns:
+    data_model_file: data_model_file (which is a list) from input, extended by the json objects from the input file(s)
+    '''
+
+    # add minimal datasets to data model file
+    if os.path.isfile(data_directory):
+        with io.open(data_directory, mode='r', encoding="utf-8") as f:
+            content = json.load(f)
+            if isinstance(content, dict):
+                data_model_file.append(content)
+            else:
+                data_model_file.extend(content)
+
+    elif os.path.isdir(data_directory):  # if it is a directory loop over all files and search for the file with the given "extension" (=pattern, can be name)
+
+        for subdir, dirs, files in os.walk(data_directory):
+            for file in files:
+                abs_result_file = os.path.join(subdir, file)
+                if fnmatch.fnmatch(file, file_extension) and os.path.isfile(abs_result_file):
+                    with io.open(abs_result_file, mode='r', encoding="utf-8") as f:
+                        content = json.load(f)
+                        if isinstance(content, dict):
+                            data_model_file.append(content)
+                        else:
+                            data_model_file.extend(content)
+
+    return data_model_file
+
+
+
+if __name__ == '__main__':
+
+    parser = ArgumentParser()
+    parser.add_argument("-v", "--validation_data", nargs="+", help="path to validated_participant_data.json", required=True)
+    parser.add_argument("-m", "--metrics_data", nargs="+", help="path to assessment_datasets.json", required=True)
+    parser.add_argument("-a", "--aggregation_data", help="dir where the data for benchmark summary/aggregation are stored",
+                        required=True)
+    parser.add_argument("-c", "--challenge_ids", help="Ids of the challenges, separated by space", nargs='+', required=True)
+    parser.add_argument("-o", "--output", help="output path where the minimal dataset JSON file will be written", required=True)
+
+    args = parser.parse_args()
+
+    main(args)
+
---
+++ summary_workflows/identification/identification_dockers/i_metrics/compute_metrics.py
@@ -0,0 +1,194 @@
+#!/usr/bin/env python3
+
+from __future__ import division
+import io
+import os
+import json
+import pandas as pd
+from argparse import ArgumentParser
+from sklearn.metrics import auc
+import JSON_templates
+import apaeval as apa
+
+def main(args):
+
+    # input parameters
+    participant_input = args.participant_data
+    gold_standards_dir = args.gold_standards_dir
+    challenge_ids = args.challenge_ids
+    participant = args.participant_name
+    community = args.community_name
+    out_path = args.output
+    windows = args.windows
+    genome_path = args.genome_dir
+
+    print(f"INFO: input {participant_input}")
+    print(f"INFO: Possible challenges {challenge_ids}")
+
+    challenge = [c for c in challenge_ids if c.split('.')[0] in str(participant_input)][0]
+    
+    print(f"INFO: Selected challenge {challenge}")
+    print(f"INFO: Window sizes {windows}")
+
+
+    # Assuring the output path does exist
+    if os.path.dirname(out_path) and not os.path.exists(os.path.dirname(out_path)):
+        try:
+            os.makedirs(os.path.dirname(out_path))
+            with open(out_path, mode="a"):
+                pass
+        except OSError as exc:
+            print("OS error: {0}".format(exc) + "\nCould not create output path: " + out_path)
+
+    compute_metrics(participant_input, gold_standards_dir, challenge, participant, community, out_path, windows, genome_path)
+
+
+def compute_metrics(infile, gold_standards_dir, challenge, participant, community, out_path, windows, genome_path):
+
+    # ID prefix for assessment objects
+    base_id = f"{community}:{challenge}:{participant}:"
+    
+    # define array that will hold the full set of assessments
+    all_assessments = []
+
+    # Dict to store metric names and corresponding variables + stderr (which is currently not computed and set to 0)
+    metrics = {}
+
+    # Vectors for AUC of precision recall curve
+    p_vec = []
+    r_vec = []
+
+    # check for presence of participant input data
+    assert os.path.exists(infile), "Participant file not found, please check input data."
+
+    # Number of PD (predicted) sites
+    PD_cnt = pd.read_csv(infile, delimiter="\t", header=None).shape[0]
+
+    # Cols for selecting unique PD sites
+    PD_cols = ['chrom_p', 'chromStart_p', 'chromEnd_p', 'strand_p']
+
+    # ground truth file
+    gold_standard = os.path.join(gold_standards_dir, challenge + ".bed")
+    assert os.path.exists(gold_standard), "Ground truth file not found, please check input data."
+
+    # Number of GT sites
+    GT_cnt = pd.read_csv(gold_standard, delimiter="\t", header=None).shape[0]
+
+    # Cols for selecting unique GT sites
+    GT_cols = ['chrom_g', 'chromStart_g', 'chromEnd_g', 'strand_g']
+
+    # genome annotation file
+    genome_file = apa.select_genome_file(challenge, genome_path)
+    print(f"INFO: In challenge {challenge}. Using genome file: {genome_file}")
+    genome = apa.load_genome(genome_file)
+    
+    # For AUC of PR-curve we need more window sizes
+    windowlist = list(range(0,max(windows),max(windows)//10))
+    # Just in case we're missing the specified windows now
+    windowlist.extend([w for w in windows if w not in windowlist])
+    windowlist.sort()
+    for window in windowlist:
+
+        # Get matching sites
+        # Beware: 1 PD site matching x GT sites will appear on x rows, and vice versa; Dropping duplicates is thus necessary in certain subsequent steps
+        matched = apa.bedtools_window(infile, gold_standard, window)
+
+        matched.sort_values(by=['chrom_p', 'chromStart_p', 'chromEnd_p', 'chromStart_g'], inplace=True, ascending=[True, True, True, True])
+
+        # METRICS for all window sizes
+        ######################################
+
+        # "true" GT: number of GT sites with matching PD sites
+        tGT_cnt = matched[~matched.duplicated(GT_cols)].shape[0]
+        # "true" PD: number of PD sites with matching GT sites
+        tPD_cnt = matched[~matched.duplicated(PD_cols)].shape[0]
+
+        # number of GT sites with matching PD site
+        TP = tGT_cnt
+        # number of prediction sites without ground truth sites
+        FP = PD_cnt - tPD_cnt
+        # number of ground truth sites without prediction sites
+        FN = GT_cnt - TP
+
+        sensitivity = apa.sensitivity(TP, FN)
+        r_vec.append(sensitivity)
+        precision = apa.precision(TP, FP)
+        p_vec.append(precision)
+        
+
+        # METRICS for specified window sizes
+        #########################################
+
+        if window in windows:
+
+            fd_rate = apa.fdr(TP, FP)
+            f1_score = apa.f1_score(precision, sensitivity)
+            jaccard_index = apa.jaccard(TP, FP, FN)
+
+            # Duplicated PD sites, i.e. PD sites that matched multiple GT sites
+            dPD_cnt = sum(matched.duplicated(PD_cols)) 
+            dPD_proportion = dPD_cnt / tPD_cnt
+
+            # Duplicated GT sites, i.e. GT sites that matched multiple PD sites
+            dGT_cnt = sum(matched.duplicated(GT_cols))
+            dGT_proportion = dGT_cnt / tGT_cnt
+
+            # Save for assessment objects
+            metrics[f"Sensitivity:{window}nt"] = [sensitivity, 0]
+            metrics[f"Precision:{window}nt"] = [precision, 0]
+            metrics[f"FDR:{window}nt"] = [fd_rate, 0]
+            metrics[f"F1_score:{window}nt"] = [f1_score, 0]
+            metrics[f"Jaccard_index:{window}nt"] = [jaccard_index, 0]
+            metrics[f"multi_matched_PD:{window}nt"] = [dPD_proportion, 0]
+            metrics[f"multi_matched_GT:{window}nt"] = [dGT_proportion, 0]
+
+    # With precision and recall for all window sizes
+    pr_auc = auc(r_vec, p_vec)
+
+    # Save for assessment objects
+    metrics["AUC"] = [pr_auc, 0] 
+
+
+    # for the challenge, create all assessment json objects and append them to all_assessments
+    for key, value in metrics.items():
+        object_id = base_id + key
+        assessment_object = JSON_templates.write_assessment_dataset(object_id, community, challenge, participant, key, value[0], value[1])
+        all_assessments.append(assessment_object)
+
+    # once all assessments have been added, print to json file
+    with io.open(out_path, mode='w', encoding="utf-8") as f:
+        jdata = json.dumps(all_assessments,                 
+                    sort_keys=True,
+                    indent=4,
+                    separators=(',', ': '))
+        f.write(jdata)
+
+    # print non-OEB-compatible metrics (lists) to separate file
+    rogue_metrics = {}
+    rogue_metrics["precision_vector"] = p_vec
+    rogue_metrics["recall_vector"] = r_vec
+    rogue_metrics["window_list"] = windowlist
+    with io.open(os.path.dirname(out_path) + "rogue_metrics.json", mode='w', encoding="utf-8") as f:
+        jdata = json.dumps(rogue_metrics,                 
+                    sort_keys=True,
+                    indent=4,
+                    separators=(',', ': '))
+        f.write(jdata)
+
+
+if __name__ == '__main__':
+
+    parser = ArgumentParser()
+    parser.add_argument("-i", "--participant_data",  help="List of execution workflow output files", required=True)
+    parser.add_argument("-c", "--challenge_ids", nargs='+', help="List of challenge ids selected by the user, separated by spaces", required=True)
+    parser.add_argument("-g", "--gold_standards_dir", help="dir that contains gold standard datasets for current challenge", required=True)
+    parser.add_argument("-p", "--participant_name", help="name of the tool used for prediction", required=True)
+    parser.add_argument("-com", "--community_name", help="name/id of benchmarking community", required=True)
+    parser.add_argument("-o", "--output", help="output path where assessment JSON files will be written", required=True)
+    parser.add_argument("-w", "--windows", nargs='+', help="windows (nt) for scanning for poly(A) sites; several window sizes separated by spaces. The first entry is used for MSE calculation.", required=True, type=int)
+    parser.add_argument("-gtf", "--genome_dir", help="genome annotation directory. Used for relative PAS usage calculation. Directory needs to contain genome files with matching organism name from challenge.", required=True)
+
+    args = parser.parse_args()
+
+
+    main(args)
