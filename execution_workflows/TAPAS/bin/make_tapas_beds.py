e8ba937fa9ee22b9ee4fda4458a0d5822fa02bc8
Farica Zhuang <faricazjj@gmail.com>
2022-09-14 15:07:34 -0400

    output relative quantification

--- execution_workflows/TAPAS/bin/make_tapas_beds.py
+++ execution_workflows/TAPAS/bin/make_tapas_beds.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
 import sys
+
 tapa_quant_txt=open(sys.argv[1],'r')
-outbed_quant=open(sys.argv[2],'w')
+outbed_relative_quant=open(sys.argv[2],'w')
 outbed_ident=open(sys.argv[3],'w')
+
 for ln in tapa_quant_txt:
  ln=ln.strip().split('\t')
  g_id,chrom,strand=ln[0].split('NM_')[1],ln[1],ln[2]
@@ -11,14 +13,14 @@ for ln in tapa_quant_txt:
  if len(sites) == len(scores):
   for i in range(len(sites)):
    name=g_id+'_'+str(i)
-   score=scores[i]
+   relative_score=scores[i]/sum(scores)
    if strand == '+':
     chromStart=sites[i]
     chromEnd=str(int(chromStart)+1)
    elif strand == '-':
     chromEnd=sites[i]
     chromStart=str(int(chromEnd)-1)
-   outbed_quant.write('\t'.join([chrom,chromStart,chromEnd,name,score,strand])+'\n')
+   outbed_relative_quant.write('\t'.join([chrom,chromStart,chromEnd,name,relative_score,strand])+'\n')
    outbed_ident.write('\t'.join([chrom,chromStart,chromEnd,name,'.',strand])+'\n')
-outbed_quant.close()
+outbed_relative_quant.close()
 outbed_ident.close()
