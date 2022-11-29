3e394ec0a04ae2198f1bbfc947e2318c57550b65
CJ Herrmann <ninsch3000@gmail.com>
2022-11-29 14:24:42 +0100

    pyranges in utils/apaeval setup

--- utils/apaeval/setup.py
+++ utils/apaeval/setup.py
@@ -3,5 +3,5 @@ from setuptools import setup, find_packages
 setup(
     packages=find_packages(where="src"),
     package_dir={'': 'src'},
-    install_requires=['scipy', 'pandas']
+    install_requires=['scipy', 'pandas', 'pyranges']
 )
