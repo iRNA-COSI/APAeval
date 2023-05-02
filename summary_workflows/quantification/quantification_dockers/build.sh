6e727d0809b42eb9ffb1ce53fa99fd92eff788c9
CJ Herrmann <ninsch3000@gmail.com>
2023-05-02 17:08:15 +0200

    fix: docker container links

--- summary_workflows/quantification/quantification_dockers/build.sh
+++ summary_workflows/quantification/quantification_dockers/build.sh
@@ -11,7 +11,7 @@ if [ $# -gt 0 ]; then
 	tag_id="$1"
 
 	for docker_name in q_validation q_metrics q_consolidation ; do
-		docker build -t apaeval/"$docker_name":"$tag_id" "$docker_name"
+		docker build -t cjh4zavolab/"$docker_name":"$tag_id" "$docker_name"
 	done
 else
 	echo "Usage: $0 tag_id" 1>&2
